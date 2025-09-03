bb5d34e16d5efe6a9621bd5ed21c4ed4825048a5
U-BSD\456523 <456523@SHS-J5W2NW3.beaverton.k12.or.us>
2025-09-03 15:37:41 -0700

    Basic outline finished

--- main.cpp
+++ main.cpp
@@ -1,1 +1,35 @@
-//whattup this is my new file
+//3 RULES OF C++:
+// - No global variables
+// - No strings
+// - Include <iostream>, not stdio, and use new and delete instead of malloc and free
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	char keepPlaying = 'y';
+	while (keepPlaying == 'y') {
+		srand(time(NULL));
+		int secretNum = rand() % 100;
+		int attempts = 0;
+		int guess;
+
+		while (true) {
+			cout << "Guess my secret number from 0 to 100!\n> ";
+			cin >> guess;
+
+			if (guess < secretNum) {
+				cout << "Rare. That number is too low.\n";
+			} else if (guess > secretNum) {
+				cout << "Burnt. That number is too high.\n";
+			} else {
+				break;
+			}
+		}
+		cout << "WELL DONE! That number is my secret number!\n";
+		cout << "Would you like to play again? (y/n)\n>";
+		cin >> keepPlaying;
+	}
+	cout << "GOODBYE!";
+}
